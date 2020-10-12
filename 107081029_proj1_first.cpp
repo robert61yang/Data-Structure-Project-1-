9c064aff067978b29d173c98a6f1cd4d66784240
107081029 <renhua61yang@gmail.com>
2020-10-13 05:55:13 +0800

    done 70%

--- 107081029_proj1_first.cpp
+++ 107081029_proj1_first.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include <string>
 
+using namespace std;
+
+int **table;
+int row, col;
 int T1[4][4] = {0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0};
 int T2[4][4] = {0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0};
 int T3[4][4] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0};
@@ -20,126 +25,86 @@ int I1[4][4] = {1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0};
 int I2[4][4] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1};
 int O[4][4] = {0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0}; 
 
-using namespace std;
+void maketable();
+void put_square(int sq[4][4],int start, int move);
+int test_overlap(int sq[4][4], int r, int c);
 
 int main()
 {
-    int i, j;
-    cout<<"T1"<<endl;
-    for(i=0;i<4;i++){
-        for(j=0;j<4;j++){
-            cout<<T1[i][j]<<" ";
-        }
-        cout<<endl;
-    }
-    cout<<"T2"<<endl;
-    for(i=0;i<4;i++){
-        for(j=0;j<4;j++){
-            cout<<T2[i][j]<<" ";
-        }
-        cout<<endl;
-    }cout<<"T3"<<endl;
-    for(i=0;i<4;i++){
-        for(j=0;j<4;j++){
-            cout<<T3[i][j]<<" ";
-        }
-        cout<<endl;
-    }cout<<"T4"<<endl;
-    for(i=0;i<4;i++){
-        for(j=0;j<4;j++){
-            cout<<T4[i][j]<<" ";
-        }
-        cout<<endl;
-    }cout<<"L1"<<endl;
-    for(i=0;i<4;i++){
-        for(j=0;j<4;j++){
-            cout<<L1[i][j]<<" ";
-        }
-        cout<<endl;
-    }cout<<"L2"<<endl;
-    for(i=0;i<4;i++){
-        for(j=0;j<4;j++){
-            cout<<L2[i][j]<<" ";
-        }
-        cout<<endl;
-    }cout<<"L3"<<endl;
-    for(i=0;i<4;i++){
-        for(j=0;j<4;j++){
-            cout<<L3[i][j]<<" ";
-        }
-        cout<<endl;
-    }cout<<"L4"<<endl;
-    for(i=0;i<4;i++){
-        for(j=0;j<4;j++){
-            cout<<L4[i][j]<<" ";
-        }
-        cout<<endl;
-    }cout<<"J1"<<endl;
-    for(i=0;i<4;i++){
-        for(j=0;j<4;j++){
-            cout<<J1[i][j]<<" ";
-        }
-        cout<<endl;
-    }cout<<"J2"<<endl;
-    for(i=0;i<4;i++){
-        for(j=0;j<4;j++){
-            cout<<J2[i][j]<<" ";
-        }
-        cout<<endl;
-    }cout<<"J3"<<endl;
-    for(i=0;i<4;i++){
-        for(j=0;j<4;j++){
-            cout<<J3[i][j]<<" ";
-        }
-        cout<<endl;
-    }cout<<"J4"<<endl;
-    for(i=0;i<4;i++){
-        for(j=0;j<4;j++){
-            cout<<J4[i][j]<<" ";
-        }
-        cout<<endl;
-    }cout<<"S1"<<endl;
-    for(i=0;i<4;i++){
-        for(j=0;j<4;j++){
-            cout<<S1[i][j]<<" ";
-        }
-        cout<<endl;
-    }cout<<"S2"<<endl;
-    for(i=0;i<4;i++){
-        for(j=0;j<4;j++){
-            cout<<S2[i][j]<<" ";
-        }
-        cout<<endl;
-    }cout<<"Z1"<<endl;
-    for(i=0;i<4;i++){
-        for(j=0;j<4;j++){
-            cout<<Z1[i][j]<<" ";
-        }
-        cout<<endl;
-    }cout<<"Z2"<<endl;
-    for(i=0;i<4;i++){
-        for(j=0;j<4;j++){
-            cout<<Z2[i][j]<<" ";
-        }
-        cout<<endl;
-    }cout<<"I1"<<endl;
-    for(i=0;i<4;i++){
-        for(j=0;j<4;j++){
-            cout<<I1[i][j]<<" ";
-        }
-        cout<<endl;
-    }cout<<"I2"<<endl;
-    for(i=0;i<4;i++){
-        for(j=0;j<4;j++){
-            cout<<I2[i][j]<<" ";
-        }
-        cout<<endl;
-    }cout<<"O"<<endl;
-    for(i=0;i<4;i++){
-        for(j=0;j<4;j++){
-            cout<<O[i][j]<<" ";
-        }
-        cout<<endl;
-    }
+    int start, move;
+    string str;
+    cin>>row>>col;
+    maketable();
     
+    while(1)
+    {   
+        cin>>str;
+        if(str == "End") break;
+        cin>>start>>move;
+        if(str == "T1"){
+            put_square(T1,start,move);
+        }
+        else if(str == "O"){
+            put_square(O,start,move);
+        }
+        
+    }
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            cout<<table[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+void maketable()
+{
+    table = new int*[row];
+    for(int i=0;i<row;i++){
+       table[i] = new int[col];
+       for(int j=0;j<col;j++){
+           table[i][j] = 0;
+       }
+    }
+}
+
+void put_square(int sq[4][4],int start, int move)
+{
+    int t_r = -1;
+    int t_c = start;
+    while(t_r<row-4)
+    {
+        if(test_overlap(sq, t_r+1, t_c)==-1) break;
+        t_r++;
+        //cout<<"t_r="<<t_r<<endl;
+    }
+    if(test_overlap(sq,t_r,t_c+move)==1){
+        t_c += move;
+        while(t_r<row-4)
+        {
+            if(test_overlap(sq, t_r+1, t_c)==-1) break;
+            t_r++;
+        }
+    }
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            if(sq[i][j] == 1) {
+                table[t_r+i][t_c+j] = 1;
+                //cout<<"table"<<t_r+i<<" "<<t_c+j<<endl;
+            }
+            
+        }
+    }
+}
+
+int test_overlap(int sq[4][4], int r, int c)
+{
+    //cout<<"test"<<r<<" "<<c<<endl;
+    for(int i=3;i>=0;i--){
+        for(int j=0;j<4;j++){
+            if(sq[i][j] == 1 && table[r+i][c+j] == 1)
+                return -1;
+        }
+    }
+    return 1;
 }
